b19a694b6c6a8f1c3fddef4913491581033b2119
Yuki Hosono <hoyuki@c1r24s10.42tokyo.jp>
2023-11-05 19:22:36 +0900

    fix mandatory2 && todo: add pfd sevral call & add func for close in pa_process

--- comands.c
+++ comands.c
@@ -6,7 +6,7 @@
 /*   By: hoyuki <hoyuki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 10:22:10 by hosonu            #+#    #+#             */
-/*   Updated: 2023/11/04 18:36:17 by hoyuki           ###   ########.fr       */
+/*   Updated: 2023/11/05 19:20:28 by hoyuki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,56 +40,63 @@ char	*path_lookup(char *envp[], t_pipex *pipex)
 	return (NULL);
 }
 
-void exec_cmd(t_pipex *pipex, char *argv[], char *envp[], int cnt)
+void	exec_cmd(t_pipex *pipex, char *argv[], char *envp[], int cnt)
 {
 	char	*path;
 
 	pipex->in_comand = argv[cnt + 2];
+	if (access(pipex->in_comand, X_OK) == 0 && argv[cnt + 2][0] == '.')
+		execve(pipex->in_comand, &pipex->in_comand, envp);
 	path = path_lookup(envp, pipex);
 	if (path == NULL)
 	{
 		perror("path");
 		exit(0);
 	}
-	execve(path, pipex->comand, envp);	
+	execve(path, pipex->comand, envp);
 }
-
-void child_process(t_pipex *pipex, int i, char *cmds[], char *envp[])
+//fix fd line 
+void	child_process(t_pipex *pipex, int i, char *cmds[], char *envp[])
 {
-	if(i == 0)
+	if (i == 0)
 	{
-		close(pipex->pp[0]);
-		dup2(pipex->pp[1], STDOUT_FILENO);
+		close(pipex->pp[0][0]);
+		dup2(pipex->pp[0][1], STDOUT_FILENO);
 		dup2(pipex->infile, STDIN_FILENO);
 	}
-	else if(i == pipex->pcnt - 1)
+	else if (i == pipex->pcnt - 1)
 	{
-		close(pipex->pp[1]);
-		dup2(pipex->pp[0], STDIN_FILENO);
+		close(pipex->pp[i][1]);
+		dup2(pipex->pp[i][0], STDIN_FILENO);
 		dup2(pipex->outfile, STDOUT_FILENO);
-	} else 
+	}
+	else
 	{
-		dup2(pipex->pp[0], STDIN_FILENO);//read from pipe
-		dup2(pipex->pp[1], STDOUT_FILENO);//write to pipe
+		dup2(pipex->pp[i - 1][0], STDIN_FILENO);
+		dup2(pipex->pp[i][1], STDOUT_FILENO);
 	}
 	exec_cmd(pipex, cmds, envp, i);
 }
 
-void run_process(t_pipex *pipex, char *cmds[], char *envp[])
+void	run_process(t_pipex *pipex, char *cmds[], char *envp[])
 {
-	int i;
+	int	i;
+
 	i = 0;
-	while(i < pipex->pcnt)
+	while (i < pipex->pcnt)
 	{
-		if(pipe(pipex->pp) == -1)
+		if (i < pipex->pcnt - 1)
 		{
-		perror("pipe");
-		exit(EXIT_FAILURE);
+			if (pipe(pipex->pp[i]) == -1)
+			{
+				perror("pipe");
+				exit(EXIT_FAILURE);
+			}
 		}
 		pipex->pid = fork();
-		if(pipex->pid == 0)
+		if (pipex->pid == 0)
 			child_process(pipex, i, cmds, envp);
-		else if(pipex->pid < 0)
+		else if (pipex->pid < 0)
 		{
 			perror("fork");
 			exit(EXIT_FAILURE);
@@ -98,11 +105,14 @@ void run_process(t_pipex *pipex, char *cmds[], char *envp[])
 	}
 }
 
-void ft_pipex(t_pipex *pipex, char **cmds, char *envp[])
+//TO DO : add func for parents process close several time
+
+
+
+void	ft_pipex(t_pipex *pipex, char **cmds, char *envp[])
 {
-	int status;
 	run_process(pipex, cmds, envp);
 	close(pipex->pp[0]);
 	close(pipex->pp[1]);
-	waitpid(pipex->pid, &status, 0);
+	waitpid(pipex->pid, NULL, 0);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hoyuki <hoyuki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 12:04:30 by hosonu            #+#    #+#             */
-/*   Updated: 2023/11/04 18:39:27 by hoyuki           ###   ########.fr       */
+/*   Updated: 2023/11/05 18:59:44 by hoyuki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	open_file(t_pipex *pipex, char *argv[], int argc)
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	t_pipex	*pipex;
+	t_pipex	pipex;
 
 	if (argc < 5)
 	{
@@ -38,12 +38,8 @@ int	main(int argc, char *argv[], char *envp[])
 		perror("argc");
 		exit(EXIT_FAILURE);
 	}
-	pipex = (t_pipex *)malloc(sizeof(t_pipex));
-	if (pipex == NULL)
-		return (0);
-	pipex->pcnt = argc - 3;
-	open_file(pipex, argv, argc);
-	ft_pipex(pipex, argv, envp);
-	free(pipex);
+	pipex.pcnt = argc - 3;
+	open_file(&pipex, argv, argc);
+	ft_pipex(&pipex, argv, envp);
 	return (0);
 }
